a1c98adf73a733d6e77c96c15b138e519d8d5aa8
Joanmi Bardera <mcontactsand@gmail.com>
2015-08-03 17:23:05 -0300

    Broadcast receiver refactored to static + launching service to run the task.

---
+++ src/com/brapeba/roaminginfo/StaticConChgReceiver.java
@@ -0,0 +1,13 @@
+package com.brapeba.roaminginfo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StaticConChgReceiver extends BroadcastReceiver 
+{
+	@Override public void onReceive(Context context, Intent intent) 
+	{
+		context.startService(new Intent(context,RoamingInfoService.class));
+	}
+}
