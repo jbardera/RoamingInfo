a60c2530114e4957f1485d0efe32ff9694d13588
Joanmi Bardera <mcontactsand@gmail.com>
2015-08-04 13:55:29 -0300

    Only display/renew notification if there is a change in the operator or country.

--- src/com/brapeba/roaminginfo/Main.java
+++ src/com/brapeba/roaminginfo/Main.java
@@ -7,14 +7,29 @@
 package com.brapeba.roaminginfo;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class Main extends Activity 
 {
+	final String PREFS = "MyPrefs";
+	SharedPreferences mySettings;
+	
 	@Override public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
+		mySettings = getSharedPreferences(PREFS, MODE_MULTI_PROCESS);
+		SharedPreferences.Editor editor = mySettings.edit();
+		editor.putString("non","null");
+		editor.putString("nci","null");
+		editor.putString("son","null");
+		editor.putString("sci","null");
+		editor.putString("non2","null");
+		editor.putString("nci2","null");
+		editor.putString("son2","null");
+		editor.putString("sci2","null");
+		editor.commit();
 		startService(new Intent(this,RoamingInfoService.class));
 		Toast.makeText(this, getString(R.string.string4), Toast.LENGTH_SHORT).show();
 		finish();
--- src/com/brapeba/roaminginfo/RoamingInfoService.java
+++ src/com/brapeba/roaminginfo/RoamingInfoService.java
@@ -1,5 +1,6 @@
 package com.brapeba.roaminginfo;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -24,7 +25,7 @@ public class RoamingInfoService extends Service
 	private boolean roaming,roaming2;
 	private String toShow,toShow2;
 	private final static int SIM1=0,SIM2=1;
-	private boolean weMtk=true;
+	private boolean weMtk=true,chgTs=false,chgTs2=false;
 	TelephonyManagerEx mgMtk;
 	NotificationManager nm;
 	StaticConChgReceiver changeReceiver;
@@ -33,61 +34,124 @@ public class RoamingInfoService extends Service
 	int ID_NOTI_ROAMING2 = 101; //2nd SIM Mediatek
 	int ID_NOTI_DATA = 110;
 	final String PREFS = "MyPrefs";
-	SharedPreferences settings;
-	
+	SharedPreferences mySettings;
+
 	@Override public void onCreate() 
 	{
 		super.onCreate();
 	}
-	
+
+	@SuppressLint("InlinedApi")
 	@SuppressWarnings("deprecation")
 	@Override public int onStartCommand(Intent intenc, int flags, int idArranque) 
 	{
 		super.onStartCommand(intenc,flags,idArranque);
-		
+
 		TelephonyManager mg = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-		toShow="[1] ";
-		try { mgMtk= new TelephonyManagerEx(this); } catch (Exception e) { weMtk=false; toShow=""; } //!weMtk in case no Mediatek 
-		
+
+		if (Build.VERSION.SDK_INT>=11) mySettings = getSharedPreferences("MyPrefs", Context.MODE_MULTI_PROCESS);
+		else mySettings = getSharedPreferences("MyPrefs",0);
+
+		String non = mySettings.getString("non","null");
+		String nci = mySettings.getString("nci","null");
+		String son = mySettings.getString("son","null");
+		String sci = mySettings.getString("sci","null");
+
 		nm= (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-		SharedPreferences mySettings;
-		
+
 		//code below for operator info (roaming)
 		carrierName = mg.getNetworkOperatorName();
 		operatorName = mg.getSimOperatorName();
 		carrierCountry = mg.getNetworkCountryIso();
 		networkOperator=mg.getNetworkOperator(); // defines the icon to show at notification
 		operatorCountryISO=mg.getSimCountryIso();
-
 		roaming = mg.isNetworkRoaming();
 		
-		if (roaming) toShow=toShow+carrierName+":"+carrierCountry+" ("+operatorName+":"+operatorCountryISO+")";
-		else toShow=toShow+carrierName+" "+networkOperator;
-		
-		if (networkOperator.length()==0) //to show NO CARRIER
-			toShow=toShow+getResources().getString(R.string.string1)+" ("+operatorName+":"+operatorCountryISO+")";
-		
+		toShow="[1] ";
+		try { mgMtk= new TelephonyManagerEx(this); } catch (Exception e) { weMtk=false; toShow=""; } //!weMtk in case no Mediatek 
+
+		chgTs=!(non.equals(carrierName))||!(nci.equals(carrierCountry))||!(son.equals(operatorName))||(!sci.equals(operatorCountryISO));
+		SharedPreferences.Editor editor = mySettings.edit();
 		PendingIntent intencionPendiente = PendingIntent.getActivity(this, 0, new Intent(this, ShutDown.class), Intent.FLAG_ACTIVITY_NO_HISTORY);
 		
-		//Notification noti = new Notification(R.drawable.stat_notify_rssi_in_range,toShow,System.currentTimeMillis());
-		
-		String uri = getResources().getString(R.string.icon_names_prefix)+networkOperator;
-		int imageResource = getResources().getIdentifier(String.valueOf(uri),"drawable", getPackageName());
-		
-		if (imageResource==0) imageResource = getResources().getIdentifier("ic_stat_notify_rssi_in_range","drawable", getPackageName());
-		
-		//Notification noti = new Notification(R.drawable.ic_stat_notify_26203,toShow,System.currentTimeMillis());
-		Notification noti = new Notification(imageResource,toShow,System.currentTimeMillis());
-		noti.setLatestEventInfo(this, toShow,getResources().getString(R.string.string3), intencionPendiente);
-		//noti.flags |= Notification.FLAG_ONGOING_EVENT; // to avoid dismiss it by swiping
-		//noti.flags |= Notification.FLAG_NO_CLEAR; // to avoid dismiss it by clear all notifications
-		nm.notify(ID_NOTI_ROAMING, noti); 
+		//only modify the notification if there is a change on what to display!
+		if (chgTs)
+		{
+			editor.putString("non",carrierName);
+			editor.putString("nci",carrierCountry);
+			editor.putString("son",operatorName);
+			editor.putString("sci",operatorCountryISO);
+			editor.commit();
+			if (roaming) toShow=toShow+carrierName+":"+carrierCountry+" ("+operatorName+":"+operatorCountryISO+")";
+			else toShow=toShow+carrierName+" "+networkOperator;
+
+			if (networkOperator.length()==0) //to show NO CARRIER
+				toShow=toShow+getResources().getString(R.string.string1)+" ("+operatorName+":"+operatorCountryISO+")";
+
+			//Notification noti = new Notification(R.drawable.stat_notify_rssi_in_range,toShow,System.currentTimeMillis());
+
+			String uri = getResources().getString(R.string.icon_names_prefix)+networkOperator;
+			int imageResource = getResources().getIdentifier(String.valueOf(uri),"drawable", getPackageName());
+
+			if (imageResource==0) imageResource = getResources().getIdentifier("ic_stat_notify_rssi_in_range","drawable", getPackageName());
+
+			//Notification noti = new Notification(R.drawable.ic_stat_notify_26203,toShow,System.currentTimeMillis());
+			Notification noti = new Notification(imageResource,toShow,System.currentTimeMillis());
+			noti.setLatestEventInfo(this, toShow,getResources().getString(R.string.string3), intencionPendiente);
+			//noti.flags |= Notification.FLAG_ONGOING_EVENT; // to avoid dismiss it by swiping
+			//noti.flags |= Notification.FLAG_NO_CLEAR; // to avoid dismiss it by clear all notifications
+			nm.notify(ID_NOTI_ROAMING, noti); 
+		}
+
+		//notification for 2nd SIM:
+		if (weMtk) 
+		{  
+			if (mgMtk.hasIccCard(SIM2)) //if SIM2 present
+			{
+				carrierName2 = mgMtk.getNetworkOperatorName(SIM2); 
+				operatorName2 = mgMtk.getSimOperatorName(SIM2);
+				carrierCountry2 = mgMtk.getNetworkCountryIso(SIM2);
+				networkOperator2=mgMtk.getNetworkOperator(SIM2);
+				operatorCountryISO2=mgMtk.getSimCountryIso(SIM2);
+				roaming2 = mgMtk.isNetworkRoaming(SIM2);
+				
+				String non2 = mySettings.getString("non2","null");
+				String nci2 = mySettings.getString("nci2","null");
+				String son2 = mySettings.getString("son2","null");
+				String sci2 = mySettings.getString("sci2","null");
+				
+				chgTs2=!(non2.equals(carrierName2))||!(nci2.equals(carrierCountry2))||!(son2.equals(operatorName2))||(!sci2.equals(operatorCountryISO2));
+
+				//again, only modify the notification if there is a change on what to display!
+				if (chgTs2)
+				{
+					editor.putString("non2",carrierName2);
+					editor.putString("nci2",carrierCountry2);
+					editor.putString("son2",operatorName2);
+					editor.putString("sci2",operatorCountryISO2);
+					editor.commit();
+					if (roaming2) toShow2="[2] "+carrierName2+":"+carrierCountry2+" ("+operatorName2+":"+operatorCountryISO2+")";
+					else toShow2="[2] "+carrierName2+" "+networkOperator2;
+
+					if (networkOperator2.length()==0) //to show NO CARRIER
+						toShow2="[2] "+getResources().getString(R.string.string1)+" ("+operatorName2+":"+operatorCountryISO2+")";
+
+					String uri2 = getResources().getString(R.string.icon_names_prefix)+networkOperator2;
+					int imageResource2 = getResources().getIdentifier(String.valueOf(uri2),"drawable", getPackageName());
+
+					if (imageResource2==0) imageResource2 = getResources().getIdentifier("ic_stat_notify_rssi_in_range","drawable", getPackageName());
+
+					//Notification noti3 = new Notification(R.drawable.ic_stat_notify_26203,toShow,System.currentTimeMillis());
+					Notification noti3 = new Notification(imageResource2,toShow2,System.currentTimeMillis());
+					noti3.setLatestEventInfo(this, toShow2,getResources().getString(R.string.string3), intencionPendiente);
+					nm.notify(ID_NOTI_ROAMING2, noti3);
+				}
+			}  
+		} 
 		
 		//code below for data connection activity
 		if (mg.getDataState()!=0) //data connection ON
 		{
-			if (Build.VERSION.SDK_INT>=11) mySettings = getSharedPreferences("MyPrefs", Context.MODE_MULTI_PROCESS);
-				else mySettings = getSharedPreferences("MyPrefs",0);
 			Boolean showme = mySettings.getBoolean("showdata",true); 
 			if (showme)
 			{
@@ -103,44 +167,15 @@ public class RoamingInfoService extends Service
 				//noti2.flags|=Notification.FLAG_SHOW_LIGHTS;
 				nm.notify(ID_NOTI_DATA,noti2);
 			}
-		} else { nm.cancel(ID_NOTI_DATA); } 
+		} else { nm.cancel(ID_NOTI_DATA); }
 		
-		//notification for 2nd SIM:
-		if (weMtk) 
-		{  
-			if (mgMtk.hasIccCard(SIM2)) //if SIM2 present
-			{
-				carrierName2 = mgMtk.getNetworkOperatorName(SIM2); 
-				operatorName2 = mgMtk.getSimOperatorName(SIM2);
-				carrierCountry2 = mgMtk.getNetworkCountryIso(SIM2);
-				networkOperator2=mgMtk.getNetworkOperator(SIM2);
-				operatorCountryISO2=mgMtk.getSimCountryIso(SIM2);
-				roaming2 = mgMtk.isNetworkRoaming(SIM2);
-				
-				if (roaming2) toShow2="[2] "+carrierName2+":"+carrierCountry2+" ("+operatorName2+":"+operatorCountryISO2+")";
-				else toShow2="[2] "+carrierName2+" "+networkOperator2;
-				
-				if (networkOperator2.length()==0) //to show NO CARRIER
-					toShow2="[2] "+getResources().getString(R.string.string1)+" ("+operatorName2+":"+operatorCountryISO2+")";
-				
-				String uri2 = getResources().getString(R.string.icon_names_prefix)+networkOperator2;
-				int imageResource2 = getResources().getIdentifier(String.valueOf(uri2),"drawable", getPackageName());
-				
-				if (imageResource2==0) imageResource2 = getResources().getIdentifier("ic_stat_notify_rssi_in_range","drawable", getPackageName());
-				
-				//Notification noti3 = new Notification(R.drawable.ic_stat_notify_26203,toShow,System.currentTimeMillis());
-				Notification noti3 = new Notification(imageResource2,toShow2,System.currentTimeMillis());
-				noti3.setLatestEventInfo(this, toShow2,getResources().getString(R.string.string3), intencionPendiente);
-				nm.notify(ID_NOTI_ROAMING2, noti3);
-			}  
-		} 
 		stopSelf();
 		return START_STICKY; 
 	}
 
 	@Override public void onDestroy() 
 	{
-	        super.onDestroy();
+		super.onDestroy();
 	}
 
 	@Override public IBinder onBind(Intent intencion) 
